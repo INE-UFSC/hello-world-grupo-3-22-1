e2e8bb429bc6dc5edef5705dcdba8b9bb78c74df
NicolasAvansi <nicolas.avansi@gmail.com>
2022-04-24 19:55:53 -0300

    Descrição do membro 2 feita

--- membro2.py
+++ membro2.py
@@ -1,2 +1,3 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Nicolas, tenho 19 anos, possuo como hobbies várias coisas, costumo jogar jogos eletrônicos e assistir séries ou animes. Tive como experiência prévia o contato com a EJ do curso, da qual faço parte já fazem alguns meses, espero aprender com os colegas de classe de entender melhor sobre como funciona a programação orientada a objetos que escuto falar em diversos lugares." 
