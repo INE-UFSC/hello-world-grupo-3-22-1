c4d30dc7b2abac802315db77bb99b082bfae87cb
marcocechinel <104157032+marcocechinel@users.noreply.github.com>
2022-04-24 22:15:18 -0300

    Descrição do membro 3 feita

--- membro3.py
+++ membro3.py
@@ -1,2 +1,3 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+    # escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+    return "Olá, eu sou o Marco, tenho 21 anos, meus principais hobbies são assistir filmes e séries, jogar jogos eletrônicos, especialmente CS:GO, e acompanhar campeonatos de futebol, basquete e CS. Meu primeiro contato com programação foi através do curso, não possuía nenhuma experiência prévia. Após alguns semestres com o curso trancado, espero gostar da disciplina e que ela me ajude a me reconectar com o curso."
